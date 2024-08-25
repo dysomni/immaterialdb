90377a1c2946f7f1743502066ce576ad0d00b2d9
james <dev@dysomni.com>
2024-08-25 13:39:59 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="immaterialdb",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(),
     install_requires=[
         "boto3",
